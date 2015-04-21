5bcfdff0959e64b62f8bdf7d82907f33917fd1bc
Andreas Ruck <mychiara@gmail.com>
2015-04-21 12:23:41 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mychiara+cookbooks@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures my-lamp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apache2', '~> 3.0.1'
 depends 'mysql', '= 5.6.1'
